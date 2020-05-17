e7c82615d7737d02d413c315da67ed0b9fd3bd25
Boyuan Liu <boyuanliu6@yahoo.com>
2020-05-17 11:15:05 -0700

    added tutorial 2

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+##########
+# Tutorial #1
+##########
+
 @app.route("/")
 def index():
     return "Hello, World!"
@@ -9,3 +13,16 @@ def index():
 @app.route("/html")
 def html():
     return render_template("index.html")
+
+
+##########
+# Tutorial #2
+##########
+
+@app.route("/hello")
+def hello():
+    return "Hello World!"
+
+@app.route("/hello/<string:name>")
+def hello_myname(name):
+    return render_template("hello.html", name=name)
