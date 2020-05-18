23cea66bd05dd583749dc7b1f5b01eb55b8e51c4
Boyuan Liu <boyuanliu6@yahoo.com>
2020-05-18 14:10:52 -0700

    added tutorial #3 contents

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+from jinja2 import environment
 app = Flask(__name__)
+
+
 
 ##########
 # Tutorial #1
@@ -26,3 +29,21 @@ def hello():
 @app.route("/hello/<string:name>")
 def hello_myname(name):
     return render_template("hello.html", name=name)
+
+##########
+# Tutorial #3
+##########
+
+def dateformat(value, format="%m-%d-%Y"):
+    return value.strftime(format)
+
+app.jinja_env.filters["dateformat"] = dateformat
+
+@app.route("/jinja")
+def jinja_demo():
+    import datetime
+    name = "DevWithMe"
+    num = 0
+    numbers = [1, 2, 3, 4, 5, 6]
+    fruits = {"apple": "red", "banana": "yellow", "orange": "orange"}
+    return render_template("jinja.html", name=name, num=num, numbers=numbers, fruits=fruits, date=datetime.datetime.now())
